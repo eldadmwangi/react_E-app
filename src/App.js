dfeb96194af68ff6ddb8fadb05fde9ab6ea01a5e
eldadmwangi <eldadmwangi@gmail.com>
2021-03-21 00:10:17 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Switch } from 'react-router-dom';
+import './App.css';
+import Button from './components/Button';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+      <BrowserRouter>
+      <Navbar/>
+     
+      </BrowserRouter>
+    
+  );
+}
+
+export default App;
